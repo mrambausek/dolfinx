75821a6915ab2373784f765e5070e2ff26b34a48
mrambausek <mrambausek@users.noreply.github.com>
2020-06-23 17:14:56 +0200

    use std::vector instead of explicit allocations

--- cpp/cmake/modules/FindKaHIP.cmake
+++ cpp/cmake/modules/FindKaHIP.cmake
@@ -92,44 +92,25 @@ if (MPI_CXX_FOUND)
       check_cxx_source_runs("
       #define MPICH_IGNORE_CXX_SEEK 1
       #include <mpi.h>
-
+      #include <vector>
       #include <kaHIP_interface.h>
 
       int main()
       {
 
       int n = 5;
-      int *xadj = new int[6];
-      xadj[0] = 0;
-      xadj[1] = 2;
-      xadj[2] = 5;
-      xadj[3] = 7;
-      xadj[4] = 9;
-      xadj[5] = 12;
-
-      int *adjncy = new int[12];
-      adjncy[0] = 1;
-      adjncy[1] = 4;
-      adjncy[2] = 0;
-      adjncy[3] = 2;
-      adjncy[4] = 4;
-      adjncy[5] = 1;
-      adjncy[6] = 3;
-      adjncy[7] = 2;
-      adjncy[8] = 4;
-      adjncy[9] = 0;
-      adjncy[10] = 1;
-      adjncy[11] = 3;
+      std::vector<int> xadj{0, 2, 5, 7, 9, 12};
+      std::vector<int> adjncy{1, 4, 0, 2, 4, 1, 3, 2, 4, 0, 1, 3};
 
       double imbalance = 0.03;
-      int *part = new int[n];
+      std::vector<int> part(n);
       int edge_cut = 0;
       int nparts = 2;
-      int *vwgt = nullptr;;
-      int *adjcwgt = nullptr;;
+      int *vwgt = nullptr;
+      int *adjcwgt = nullptr;
 
-      kaffpa(&n, vwgt, xadj, adjcwgt, adjncy, &nparts, &imbalance, false, 0, ECO,
-             &edge_cut, part);
+      kaffpa(&n, vwgt, xadj.data(), adjcwgt, adjncy.data(), &nparts, &imbalance, false, 0, ECO,
+             &edge_cut, part.data());
 
       return 0;
       }
